a3ef3cc87968d0c0e5b0fe81066424aa681f7a06
811324104058-Jenita <811324104058@jjcet.ac.in>
2026-08-15 09:05:49 +0530

    Added new file

---
+++ backend/src/main/java/com/smartsociety/smart_society/SmartSocietyApplication.java
@@ -0,0 +1,13 @@
+package com.smartsociety.smart_society;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSocietyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/smart/society/example/smart/society/SmartSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package smart.society.example.smart.society;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
