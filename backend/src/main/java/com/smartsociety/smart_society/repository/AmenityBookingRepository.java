aa2a78dc4208b558d5484efc69e8a0ff0dc138ef
811324104058-Jenita <811324104058@jjcet.ac.in>
2026-09-19 12:43:48 +0530

    Update amenity booking repository

--- backend/src/main/java/com/smartsociety/smart_society/repository/AmenityBookingRepository.java
+++ backend/src/main/java/com/smartsociety/smart_society/repository/AmenityBookingRepository.java
@@ -1,14 +1,18 @@
 package com.smartsociety.smart_society.repository;
 
-import java.util.List;
-
+import com.smartsociety.smart_society.entity.AmenityBooking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.smartsociety.smart_society.entity.AmenityBooking;
+import java.time.LocalDate;
+import java.util.List;
 
-public interface AmenityBookingRepository
-        extends JpaRepository<AmenityBooking, Long> {
+public interface AmenityBookingRepository extends JpaRepository<AmenityBooking, Long> {
 
     List<AmenityBooking> findByResidentEmail(String residentEmail);
 
+    List<AmenityBooking> findByAmenityIdAndBookingDateAndBookingStatus(
+            Long amenityId,
+            LocalDate bookingDate,
+            String bookingStatus
+    );
 }
